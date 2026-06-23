82581b8956721166f88382b84e6b5f00ed08117a
ramasamy <vicky@gmail.com>
2026-06-23 19:47:16 +0530

    craeted files

--- src/App.js
+++ src/App.js
@@ -1,63 +1,100 @@
 import React, { useState } from "react";
 import "./App.css";
-
+import img1 from "./image/neodium.webp";
+import img2 from "./image/neodium magnet.jpg";
+import img3 from "./image/disco-ima-neodimio-n35-niquel-20x1-5-mm-imashop-01.webp";
+import img4 from "./image/neodymium-rare-earth-disc-magnet-500x500.webp";
+import img5 from "./image/magnet .jpeg";
+import img6 from "./image/OIP.jpeg";
+import img7 from "./image/logo.jpeg"
 function App() {
+  const [cart, setCart] = useState([]);
+
   const products = [
     {
-      id: 1,
-      name: "Laptop",
-      price: 50000,
-      image:
-        "https://images.unsplash.com/photo-1496181133206-80ce9b88a853"
+      id:"1",
+       name:"Magnet1",
+         size: "25*5", 
+      grade:"N-35",
+      price: "500", 
+      image: img1
     },
-    {
+    { 
       id: 2,
-      name: "Mobile",
-      price: 20000,
-      image:
-        "https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
-    },
+       name:"Magnet2",
+      size: "10*2", 
+      grade:"N-35",
+      price: 100,
+       image: img2
+     },
     {
-      id: 3,
-      name: "Headphone",
-      price: 3000,
-      image:
-        "https://images.unsplash.com/photo-1505740420928-5e560c06d30e"
-    }
-  ];
+       id: 3,
+        name:"Magnet3",
+       size: "10*10*2",
+      grade:"N-35",
+       price: 250,
+          image: img3
+      },
+         { 
+          id: 4, 
+           name:"Magnet4",
+      size: "5*3", 
+      grade:"N-35",
+      price: 100,
+         image: img4
 
-  const [cart, setCart] = useState([]);
+    },
+        {
+           id: 5,
+             name:"Magnet5",
+      size: "6*3", 
+      grade:"N-35",
+      price: 100,
+         image: img5
+     },
+        { 
+          id: 6, 
+ name:"Magnet6",
+      size: "40*10*5", 
+      grade:"N-35",
+      price: 100,
+         image: img6
+    },
+    
+  ];
 
   const addToCart = (product) => {
     setCart([...cart, product]);
   };
 
   return (
-    <div>
-      
-      <nav className="navbar">
-        <h2>MyShop</h2>
-        <h3> Cart: {cart.length}</h3>
-      </nav>
+    <div className="header">
+      <img src={img7} alt="logo" className="logo" />
+      <h1></h1>   
 
-      
-      <div className="product-container">
+      <h2>Cart Items: {cart.length}</h2>
+
+      <div className="products-gride">
         {products.map((product) => (
-          <div className="card" key={product.id}>
-            <img src={product.image} alt={product.name} />
-            <h3>{product.name}</h3>
-            <p>₹{product.price}</p>
-            <p style={{color:"green"}}>
-  20% OFF
-</p>
+          < div className="card" key={product.id}>
+            <img
+  src={product.image}
+  alt={product.name}
+  className="product-image"
+/>
+    <h3>{product.name}</h3>
+    <p>{product.size}</p>
+    <p>{product.grade}</p>
+    <p>₹{product.price}</p>
 
             <button onClick={() => addToCart(product)}>
-              Add To Cart
+              Add to Cart
             </button>
           </div>
         ))}
       </div>
     </div>
+    
   );
 }
 
